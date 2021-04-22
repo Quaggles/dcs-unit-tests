036e15a724edb74054edc7fda61f1f212b848aa6
Recoil016 <recoil@recoil.nrw>
2021-04-22 20:10:16 +0200

    Add A-10C_2 AGM-65

---
+++ A-10C_2/AGM-65/Force Correlated/payloads.lua
@@ -0,0 +1,32 @@
+payloads = {
+	["AGM-65H LAU-117"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "LAU_117_AGM_65H",},
+        }
+    },
+	["AGM-65H x1 LAU-88"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "LAU_88_AGM_65H",},
+        }
+    },
+	["AGM-65H x2 LAU-88"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "LAU_88_AGM_65H_2_R",},
+        }
+    },
+	["AGM-65H x3 LAU-88"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "LAU_88_AGM_65H_3",},
+        }
+    },
+	["AGM-65G LAU-117"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "LAU_117_AGM_65G",},
+        }
+    },
+	["AGM-65K LAU-117"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "{69DC8AE7-8F77-427B-B8AA-B19D3F478B66}",},
+        }
+    },
+}
---
+++ A-10C_2/AGM-65/Normal/payloads.lua
@@ -0,0 +1,52 @@
+payloads = {
+    ["AGM-65D LAU-117"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "{444BA8AE-82A7-4345-842E-76154EFCCA46}",},
+        }
+    },
+	["AGM-65D x1 LAU-88"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "LAU_88_AGM_65D_ONE",},
+        }
+    },
+	["AGM-65D x2 LAU-88"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "{E6A6262A-CA08-4B3D-B030-E1A993B98452}",},
+        }
+    },
+	["AGM-65D x3 LAU-88"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "{DAC53A2F-79CA-42FF-A77A-F5649B601308}",},
+        }
+    },
+	["AGM-65H LAU-117"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "LAU_117_AGM_65H",},
+        }
+    },
+	["AGM-65H x1 LAU-88"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "LAU_88_AGM_65H",},
+        }
+    },
+	["AGM-65H x2 LAU-88"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "LAU_88_AGM_65H_2_R",},
+        }
+    },
+	["AGM-65H x3 LAU-88"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "LAU_88_AGM_65H_3",},
+        }
+    },
+	["AGM-65G LAU-117"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "LAU_117_AGM_65G",},
+        }
+    },
+	["AGM-65K LAU-117"] = {
+        ["pylons"] = {
+            [9] = {["CLSID"] = "{69DC8AE7-8F77-427B-B8AA-B19D3F478B66}",},
+        }
+    },
+}

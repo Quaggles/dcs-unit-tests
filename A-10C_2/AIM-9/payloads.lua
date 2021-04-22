ad66bdcd8df8a9069d56e1ddbc37bc7088b48a50
Recoil016 <recoil@recoil.nrw>
2021-04-22 20:07:42 +0200

    Add A-10C_2 AIM-9

---
+++ A-10C_2/AIM-9/payloads.lua
@@ -0,0 +1,12 @@
+payloads = {
+    ["AIM-9M"] = {
+        ["pylons"] = {
+            [11] = {["CLSID"] = "{DB434044-F5D0-4F1F-9BA9-B73027E18DD3}",},
+        }
+    },
+	["AIM-9L"] = {
+        ["pylons"] = {
+            [11] = {["CLSID"] = "LAU-105_2*AIM-9L",},
+        }
+    },
+}

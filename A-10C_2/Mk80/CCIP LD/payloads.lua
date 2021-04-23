5d8dc404fa1c37934d50ca06acef82aba4ad36b2
Recoil016 <recoil@recoil.nrw>
2021-04-23 19:13:12 +0200

    Add A-10C_2 Mk80 CCIP LD

---
+++ A-10C_2/Mk80/CCIP LD/payloads.lua
@@ -0,0 +1,18 @@
+payloads = {
+    ["Mk82"] = {
+        ["pylons"] = {
+            [6] = {["CLSID"] = "{BCE4E030-38E9-423E-98ED-24BE3DA87C32}",},
+        }
+    },
+	["Mk82 x3"] = {
+        ["pylons"] = { --2nd rack as a counterweight to have consistency with the other payloads
+            [5] = {["CLSID"] = "{60CC734F-0AFA-4E2E-82B8-93B941AB11CF}",},
+            [7] = {["CLSID"] = "{60CC734F-0AFA-4E2E-82B8-93B941AB11CF}",},
+        }
+    },
+	["Mk84"] = {
+        ["pylons"] = {
+            [6] = {["CLSID"] = "{AB8B8299-F1CC-4359-89B5-2172E0CF4A5A}",},
+        }
+    },
+}

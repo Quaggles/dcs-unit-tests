f449782df7a0ee631a8a13bc355e39c67bc601bf
Recoil016 <recoil@recoil.nrw>
2021-04-24 15:57:50 +0200

    Added A-10C_2 Mk80 CCIP HD

---
+++ A-10C_2/Mk80/CCIP HD/payloads.lua
@@ -0,0 +1,13 @@
+payloads = {
+    ["Mk82AIR"] = {
+        ["pylons"] = {
+            [6] = {["CLSID"] = "{Mk82AIR}",},
+        }
+    },
+	["Mk82AIR x3"] = {
+        ["pylons"] = { --2nd rack as a counterweight to have consistency with the other payloads
+            [5] = {["CLSID"] = "{BRU-42_3*Mk-82AIR}",},
+            [7] = {["CLSID"] = "{BRU-42_3*Mk-82AIR}",},
+        }
+    },
+}

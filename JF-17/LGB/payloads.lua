b9ad79f5b5851f5fdf2a11519f93599b2bf6149f
Quaggles <RyanQuagliata@gmail.com>
2022-07-02 19:34:37 +1000

    Updated JF-17 tests

---
+++ JF-17/LGB/payloads.lua
@@ -0,0 +1,24 @@
+payloads = {
+	["GBU16"] = {
+        ["pylons"] = {
+            [3] = {["CLSID"] = "DIS_GBU_16",},
+			[4] = {["CLSID"] = "DIS_WMD7",},
+			[5] = {["CLSID"] = "DIS_GBU_16",},
+        }
+    },
+	["GBU10"] = {
+        ["pylons"] = {
+            [3] = {["CLSID"] = "DIS_GBU_10",},
+			[4] = {["CLSID"] = "DIS_WMD7",},
+			[5] = {["CLSID"] = "DIS_GBU_10",},
+        }
+    },
+	["GBU12"] = {
+        ["pylons"] = {
+            [3] = {["CLSID"] = "DIS_GBU_12",},
+			[4] = {["CLSID"] = "DIS_WMD7",},
+			[5] = {["CLSID"] = "DIS_GBU_12",},
+        }
+    },
+}
+

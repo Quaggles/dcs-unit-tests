d6872bb6c68a931b1ce0a0ff1cf10c2484d5d777
Quaggles <RyanQuagliata@gmail.com>
2022-07-05 12:48:49 +1000

    Added more F-16 tests from ShadowFrost

---
+++ F-16/AIM-120/Supported/payloads.lua
@@ -0,0 +1,15 @@
+payloads = {
+	["AIM120CSupported"] = {
+        ["pylons"] = {
+            [1] = {["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",},
+			[9] = {["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",},
+        }
+    },
+	["AIM120BSupported"] = {
+        ["pylons"] = {
+            [1] = {["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",},
+			[2] = {["CLSID"] = "{C8E06185-7CD6-4C90-959F-044679E90751}",},
+        }
+    },
+}
+

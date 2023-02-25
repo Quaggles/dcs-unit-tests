ad4cef683a1e89bc7ff659a19ec52603557ee55a
Quaggles <RyanQuagliata@gmail.com>
2023-02-26 07:52:24 +1000

    Rerecorded AIM-54 Maddog and PD-STT tests, added AIM-54C Mk60 variant

--- ThirdParty/F-14/AIM-54/payloads.lua
+++ ThirdParty/F-14/AIM-54/payloads.lua
@@ -1,17 +1,22 @@
 payloads = {
-    ["HotAMk47"] = {
+    ["AIM-54A Mk47"] = {
         ["pylons"] = {
             [6] = {["CLSID"] = "{AIM_54A_Mk47}",},
         }
     },
-    ["HotAMk60"] = {
+    ["AIM-54A Mk60"] = {
         ["pylons"] = {
             [6] = {["CLSID"] = "{AIM_54A_Mk60}",},
         }
     },
-    ["HotCmk47"] = {
+    ["AIM-54C Mk47"] = {
         ["pylons"] = {
             [6] = {["CLSID"] = "{AIM_54C_Mk47}",},
+        }
+    },
+    ["AIM-54C Mk60"] = {
+        ["pylons"] = {
+            [6] = {["CLSID"] = "{AIM_54C_Mk60}",},
         }
     },
 }

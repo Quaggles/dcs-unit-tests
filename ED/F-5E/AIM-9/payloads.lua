c53f9f84ebbc8bf38d09b20b41e540f98fb508aa
Quaggles <RyanQuagliata@gmail.com>
2023-03-09 17:33:09 +1000

    Reorganised F-5E tests

---
+++ ED/F-5E/AIM-9/payloads.lua
@@ -0,0 +1,20 @@
+payloads = {
+    ["AIM-9B"] = {
+        ["pylons"] = {
+            [1] = {["CLSID"] = "{AIM-9B}",},
+            [7] = {["CLSID"] = "{AIM-9B}",},
+        }
+    },
+    ["AIM-9P"] = {
+        ["pylons"] = {
+            [1] = {["CLSID"] = "{9BFD8C90-F7AE-4e90-833B-BFD0CED0E536}",},
+            [7] = {["CLSID"] = "{9BFD8C90-F7AE-4e90-833B-BFD0CED0E536}",},
+        }
+    },
+    ["AIM-9P5"] = {
+        ["pylons"] = {
+            [1] = {["CLSID"] = "{AIM-9P5}",},
+            [7] = {["CLSID"] = "{AIM-9P5}",},
+        }
+    },
+}

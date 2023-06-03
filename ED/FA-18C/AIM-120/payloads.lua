d16ff45ab86f2b859ca712bba863211693f94c46
Quaggles <RyanQuagliata@gmail.com>
2023-06-03 21:18:52 +1000

    F/A-18C removed obsolete LAU-127 pylon tests

--- ED/FA-18C/AIM-120/payloads.lua
+++ ED/FA-18C/AIM-120/payloads.lua
@@ -11,12 +11,6 @@ payloads = {
             [3] = {["CLSID"] = "{LAU-115 - AIM-120B}",},
         }
     },
-    ["AIM-120B LAU-127"] = {
-        ["pylons"] = {
-            [7] = {["CLSID"] = "LAU-115_2*LAU-127_AIM-120B",},
-            [3] = {["CLSID"] = "LAU-115_2*LAU-127_AIM-120B",},
-        }
-    },
     ["AIM-120C Cheek"] = {
         ["pylons"] = {
             [6] = {["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",},
@@ -27,12 +21,6 @@ payloads = {
         ["pylons"] = {
             [7] = {["CLSID"] = "{LAU-115 - AIM-120C}",},
             [3] = {["CLSID"] = "{LAU-115 - AIM-120C}",},
-        }
-    },
-    ["AIM-120C LAU-127"] = {
-        ["pylons"] = {
-            [7] = {["CLSID"] = "LAU-115_2*LAU-127_AIM-120C",},
-            [3] = {["CLSID"] = "LAU-115_2*LAU-127_AIM-120C",},
         }
     },
 }

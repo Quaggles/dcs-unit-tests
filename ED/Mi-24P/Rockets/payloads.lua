ecd71003e9e3825ca2fe78c5da100d101c30d428
DeMonteur <herna.pavol@gmail.com>
2023-07-12 19:23:05 +0200

    Mi-24 Tests
    Finally I've finished Mi-24 tests it is 52 test for all Mi-24 weapons except S-8TsM orange smoke rockets since I haven't found a way to test it.
    There are 2 failed tests of 2XR60 Missiles because of reported double fire bug when the Fire button is uncovered. More info here: https://forum.dcs.world/topic/325936-mi-24-r-60-double-fire/#comment-5218622https://forum.dcs.world/topic/325936-mi-24-r-60-double-fire/#comment-5218622

---
+++ ED/Mi-24P/Rockets/payloads.lua
@@ -0,0 +1,164 @@
+
+payloads = {
+	 	["Mi-24_S13-OF"] = {
+			["pylons"] = {
+				[2] =
+	        		{
+					["CLSID"] = "{FC56DF80-9B09-44C5-8976-DCFAFF219062}" --S13OF
+	        		}, -- end of [2]
+				[5] =
+	        		{
+					["CLSID"] = "{FC56DF80-9B09-44C5-8976-DCFAFF219062}" --S13OF
+	        		}, -- end of [5]
+			}
+		},
+
+		--S5-KO in UB-32A-24 pods
+	 	["Mi-24_S5KO_in"] = {
+			["pylons"] = {
+				[3] =
+	        		{
+					["CLSID"] = "{UB-32A-24}" --S5KO
+	        		}, -- end of [3]
+				[4] =
+	        		{
+					["CLSID"] = "{UB-32A-24}" --S5KO
+	        		}, -- end of [4]
+			}
+		},
+	 	["Mi-24_S5KO_out"] = {
+			["pylons"] = {
+				[2] =
+	        		{
+					["CLSID"] = "{UB-32A-24}" --S5KO
+	        		}, -- end of [2]
+				[5] =
+	        		{
+					["CLSID"] = "{UB-32A-24}" --S5KO
+	        		}, -- end of [5]
+			}
+		},
+	 	["Mi-24_S5KO_all"] = {
+			["pylons"] = {
+				[2] =
+	        		{
+					["CLSID"] = "{UB-32A-24}" --S5KO
+	        		}, -- end of [2]
+				[3] =
+	        		{
+					["CLSID"] = "{UB-32A-24}" --S5KO
+	        		}, -- end of [3]
+				[4] =
+	        		{
+					["CLSID"] = "{UB-32A-24}" --S5KO
+	        		}, -- end of [4]
+				[5] =
+	        		{
+					["CLSID"] = "{UB-32A-24}" --S5KO
+	        		}, -- end of [5]
+			}
+		},
+
+
+		--S8-KOM 
+	 	["Mi-24_S8KOM_in"] = {
+			["pylons"] = {
+				[3] =
+	        		{
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}" --S8KOM
+	        		}, -- end of [3]
+				[4] =
+	        		{
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}" --S8KOM
+	        		}, -- end of [4]
+			}
+		},
+	 	["Mi-24_S8KOM_out"] = {
+			["pylons"] = {
+				[2] =
+	        		{
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}" --S8KOM
+	        		}, -- end of [2]
+				[5] =
+	        		{
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}" --S8KOM
+	        		}, -- end of [5]
+			}
+		},
+	 	["Mi-24_S8KOM_all"] = {
+			["pylons"] = {
+				[2] =
+	        		{
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}" --S8KOM
+	        		}, -- end of [2]
+				[3] =
+	        		{
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}" --S8KOM
+	        		}, -- end of [3]
+				[4] =
+	        		{
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}" --S8KOM
+	        		}, -- end of [4]
+				[5] =
+	        		{
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}" --S8KOM
+	        		}, -- end of [5]
+			}
+		},
+
+
+
+		--S8OFP2
+		["Mi-24_S8OFP2_in"] = {
+			["pylons"] = {
+				[3] =
+	        		{
+					["CLSID"] = "B_8V20A_OFP2" --S8OFP2
+	        		}, -- end of [3]
+				[4] =
+	        		{
+					["CLSID"] = "B_8V20A_OFP2" --S8OFP2
+	        		}, -- end of [4]
+			}
+		},
+	 	["Mi-24_S8OFP2_out"] = {
+			["pylons"] = {
+				[2] =
+	        		{
+					["CLSID"] = "B_8V20A_OFP2" --S8OFP2
+	        		}, -- end of [2]
+				[5] =
+	        		{
+					["CLSID"] = "B_8V20A_OFP2" --S8OFP2
+	        		}, -- end of [5]
+			}
+		},
+	 	["Mi-24_S8OFP2_all"] = {
+			["pylons"] = {
+				[2] =
+	        		{
+					["CLSID"] = "B_8V20A_OFP2" --S8OFP2
+	        		}, -- end of [2]
+				[3] =
+	        		{
+					["CLSID"] = "B_8V20A_OFP2" --S8OFP2
+	        		}, -- end of [3]
+				[4] =
+	        		{
+					["CLSID"] = "B_8V20A_OFP2" --S8OFP2
+	        		}, -- end of [4]
+				[5] =
+	        		{
+					["CLSID"] = "B_8V20A_OFP2" --S8OFP2
+	        		}, -- end of [5]
+			}
+		},
+}
+
+
+
+--Rockets CLSIDs
+--["CLSID"] = "{FC56DF80-9B09-44C5-8976-DCFAFF219062}" --S-13-OF
+--["CLSID"] = "{UB-32A-24}" --S5KO
+--["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}" --S-8-KOM
+--["CLSID"] = "B_8V20A_OFP2" --S-8OFP2

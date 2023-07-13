f8cf1f6cba567cbf617163320204e047c8806786
DeMonteur <139075129+DeMonteur@users.noreply.github.com>
2023-07-13 12:11:37 +0200

    Mi-24 GUV8700 Guns test changed TGT position (#11)
    * L-Gun track on Mi8
    
    Corrected L Gunner track on Mi8
    
    * Update of Mi-8 L gunner as playertrackfile
    
    Fire the one long burst to keep an eye on that burst issue we found
    
    * Revert "Update of Mi-8 L gunner as playertrackfile"
    
    This reverts commit 6634a4d50e475bb418e5b559aadb6179e3a80788.
    
    * Mi-24 Tests
    
    Finally I've finished Mi-24 tests it is 52 test for all Mi-24 weapons except S-8TsM orange smoke rockets since I haven't found a way to test it.
    There are 2 failed tests of 2XR60 Missiles because of reported double fire bug when the Fire button is uncovered. More info here: https://forum.dcs.world/topic/325936-mi-24-r-60-double-fire/#comment-5218622https://forum.dcs.world/topic/325936-mi-24-r-60-double-fire/#comment-5218622
    
    * Changet TGT pos
    
    Tracks with higher situated targed due failing tests in new CB 2.8.7.42161
    Not sure if it is bug or feature.

---
+++ ED/Mi-24P/GunPods/GUV8700GR/payloads.lua
@@ -0,0 +1,54 @@
+
+payloads = {
+	 	["Mi-24_GUV-8700-30mm_Gren_out"] = {
+			["pylons"] = {
+				[2] =
+	        		{
+					["CLSID"] = "GUV_VOG" --GUV-8700 30mm grenade launcher
+	        		}, -- end of [2]
+				[5] =
+	        		{
+					["CLSID"] = "GUV_VOG" --GUV-8700 30mm grenade launcher
+	        		}, -- end of [5]
+			}
+		},
+	 	["Mi-24_GUV-8700-30mm_Gren_in"] = {
+			["pylons"] = {
+				[3] =
+	        		{
+					["CLSID"] = "GUV_VOG" --GUV-8700 30mm grenade launcher
+	        		}, -- end of [3]
+				[4] =
+	        		{
+					["CLSID"] = "GUV_VOG" --GUV-8700 30mm grenade launcher
+	        		}, -- end of [4]
+			}
+		},
+	 	["Mi-24_GUV-8700-30mm_Gren_all"] = {
+			["pylons"] = {
+				[2] =
+	        		{
+					["CLSID"] = "GUV_VOG" --GUV-8700 30mm grenade launcher
+	        		}, -- end of [2]
+				[3] =
+	        		{
+					["CLSID"] = "GUV_VOG" --GUV-8700 30mm grenade launcher
+	        		}, -- end of [3]
+				[4] =
+	        		{
+					["CLSID"] = "GUV_VOG" --GUV-8700 30mm grenade launcher
+	        		}, -- end of [4]
+				[5] =
+	        		{
+					["CLSID"] = "GUV_VOG" --GUV-8700 30mm grenade launcher
+	        		}, -- end of [5]
+			}
+		},
+
+		}
+
+
+
+--Gunpods CLSIDs
+--["CLSID"] = "GUV_VOG" --GUV-8700 30mm grenade launcher
+--["CLSID"] = "GUV_YakB_GSHP" --GUV-8700 12,7mm gun and 2x7,62 guns

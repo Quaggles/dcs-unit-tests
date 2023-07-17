461327e690e16c9a83f575d552e7c850c3fd0d53
DeMonteur <herna.pavol@gmail.com>
2023-07-17 12:05:14 +0200

    Removed all the possible colors on stores to keep just one

--- ED/Mi-8MTV2/Rockets/S8TsM/payloads.lua
+++ ED/Mi-8MTV2/Rockets/S8TsM/payloads.lua
@@ -1,139 +1,139 @@
 
 payloads = {
-		--S8TsM Blue
-		["Mi-8_S8TsM_in_blue"] = {
-			["pylons"] = {
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [4]
-			}
-		},
-		["Mi-8_S8TsM_mid_blue"] = {
-			["pylons"] = {
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [2]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [5]
-			}
-		},
-	 	["Mi-8_S8TsM_out_blue"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [1]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
-	 	["Mi-8_S8TsM_all_blue"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [1]
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [2]
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [4]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [5]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
-
-
-		-------------
-		--S8TsM Green
-		-------------
-		["Mi-8_S8TsM_in_green"] = {
-			["pylons"] = {
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [4]
-			}
-		},
-		["Mi-8_S8TsM_mid_green"] = {
-			["pylons"] = {
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [2]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [5]
-			}
-		},
-	 	["Mi-8_S8TsM_out_green"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [1]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
-	 	["Mi-8_S8TsM_all_green"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [1]
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [2]
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [4]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [5]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
+--		--S8TsM Blue
+--		["Mi-8_S8TsM_in_blue"] = {
+--			["pylons"] = {
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [4]
+--			}
+--		},
+--		["Mi-8_S8TsM_mid_blue"] = {
+--			["pylons"] = {
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [2]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [5]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_out_blue"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [1]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_all_blue"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [1]
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [2]
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [4]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [5]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_BU" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
+--
+--
+--		-------------
+--		--S8TsM Green
+--		-------------
+--		["Mi-8_S8TsM_in_green"] = {
+--			["pylons"] = {
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [4]
+--			}
+--		},
+--		["Mi-8_S8TsM_mid_green"] = {
+--			["pylons"] = {
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [2]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [5]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_out_green"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [1]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_all_green"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [1]
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [2]
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [4]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [5]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_GN" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
 		-------------
 		--S8TsM Orange
 		-------------
@@ -201,274 +201,274 @@ payloads = {
 	        		}, -- end of [6]
 			}
 		},
-		-------------
-		--S8TsM Red
-		-------------
-		["Mi-8_S8TsM_in_red"] = {
-			["pylons"] = {
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [4]
-			}
-		},
-		["Mi-8_S8TsM_mid_red"] = {
-			["pylons"] = {
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [2]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [5]
-			}
-		},
-	 	["Mi-8_S8TsM_out_red"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [1]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
-	 	["Mi-8_S8TsM_all_red"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [1]
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [2]
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [4]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [5]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
-		-------------
-		--S8TsM Violet
-		-------------
-		["Mi-8_S8TsM_in_violet"] = {
-			["pylons"] = {
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [4]
-			}
-		},
-		["Mi-8_S8TsM_mid_violet"] = {
-			["pylons"] = {
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [2]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [5]
-			}
-		},
-	 	["Mi-8_S8TsM_out_violet"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [1]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
-	 	["Mi-8_S8TsM_all_violet"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [1]
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [2]
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [4]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [5]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
-		-------------
-		--S8TsM White
-		-------------
-		["Mi-8_S8TsM_in_white"] = {
-			["pylons"] = {
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [4]
-			}
-		},
-		["Mi-8_S8TsM_mid_white"] = {
-			["pylons"] = {
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [2]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [5]
-			}
-		},
-	 	["Mi-8_S8TsM_out_white"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [1]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
-	 	["Mi-8_S8TsM_all_white"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [1]
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [2]
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [4]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [5]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
-		-------------
-		--S8TsM Yellow
-		-------------
-		["Mi-8_S8TsM_in_yellow"] = {
-			["pylons"] = {
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [4]
-			}
-		},
-		["Mi-8_S8TsM_mid_yellow"] = {
-			["pylons"] = {
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [2]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [5]
-			}
-		},
-	 	["Mi-8_S8TsM_out_yellow"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [1]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
-	 	["Mi-8_S8TsM_all_yellow"] = {
-			["pylons"] = {
-				[1] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [1]
-				[2] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [2]
-				[3] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [3]
-				[4] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [4]
-				[5] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [5]
-				[6] =
-	        		{
-					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
-	        		}, -- end of [6]
-			}
-		},
+--		-------------
+--		--S8TsM Red
+--		-------------
+--		["Mi-8_S8TsM_in_red"] = {
+--			["pylons"] = {
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [4]
+--			}
+--		},
+--		["Mi-8_S8TsM_mid_red"] = {
+--			["pylons"] = {
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [2]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [5]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_out_red"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [1]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_all_red"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [1]
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [2]
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [4]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [5]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_RD" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
+--		-------------
+--		--S8TsM Violet
+--		-------------
+--		["Mi-8_S8TsM_in_violet"] = {
+--			["pylons"] = {
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [4]
+--			}
+--		},
+--		["Mi-8_S8TsM_mid_violet"] = {
+--			["pylons"] = {
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [2]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [5]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_out_violet"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [1]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_all_violet"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [1]
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [2]
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [4]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [5]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_VT" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
+--		-------------
+--		--S8TsM White
+--		-------------
+--		["Mi-8_S8TsM_in_white"] = {
+--			["pylons"] = {
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [4]
+--			}
+--		},
+--		["Mi-8_S8TsM_mid_white"] = {
+--			["pylons"] = {
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [2]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [5]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_out_white"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [1]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_all_white"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [1]
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [2]
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [4]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [5]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_WH" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
+--		-------------
+--		--S8TsM Yellow
+--		-------------
+--		["Mi-8_S8TsM_in_yellow"] = {
+--			["pylons"] = {
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [4]
+--			}
+--		},
+--		["Mi-8_S8TsM_mid_yellow"] = {
+--			["pylons"] = {
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [2]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [5]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_out_yellow"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [1]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
+--	 	["Mi-8_S8TsM_all_yellow"] = {
+--			["pylons"] = {
+--				[1] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [1]
+--				[2] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [2]
+--				[3] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [3]
+--				[4] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [4]
+--				[5] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [5]
+--				[6] =
+--	        		{
+--					["CLSID"] = "B_8V20A_CM_YE" --S8TsM
+--	        		}, -- end of [6]
+--			}
+--		},
 }
 
 

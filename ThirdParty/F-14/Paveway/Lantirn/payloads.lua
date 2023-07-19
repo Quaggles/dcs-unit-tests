b9ee821aef0c1a4aba591aeb061792b838f6b847
Quaggles <RyanQuagliata@gmail.com>
2023-07-20 01:23:09 +1000

    Fixed broken LANTIRN tests

--- ThirdParty/F-14/Paveway/Lantirn/payloads.lua
+++ ThirdParty/F-14/Paveway/Lantirn/payloads.lua
@@ -2,19 +2,19 @@ payloads = {
     ["GBU-10"] = {
         ["pylons"] = {
             [7] = {["CLSID"] = "{BRU-32 GBU-10}",},
-            [9] = {["CLSID"] = "{{F14-LANTIRN-TP}}",},
+            [9] = {["CLSID"] = "{F14-LANTIRN-TP}",},
         }
     },
     ["GBU-12"] = {
         ["pylons"] = {
             [7] = {["CLSID"] = "{BRU-32 GBU-12}",},
-            [9] = {["CLSID"] = "{{F14-LANTIRN-TP}}",},
+            [9] = {["CLSID"] = "{F14-LANTIRN-TP}",},
         }
     },
     ["GBU-16"] = {
         ["pylons"] = {
             [7] = {["CLSID"] = "{BRU-32 GBU-16}",},
-            [9] = {["CLSID"] = "{{F14-LANTIRN-TP}}",},
+            [9] = {["CLSID"] = "{F14-LANTIRN-TP}",},
         }
     },
 }

9779f30eabc5c1253cfecd0438887e1218c9ea49
Quaggles <RyanQuagliata@gmail.com>
2023-07-19 18:23:50 +1000

    Moved some F/A-18C tests to regression testing level

--- ED/FA-18C/AGM-154/payloads.lua
+++ ED/FA-18C/AGM-154/payloads.lua
@@ -4,7 +4,7 @@ payloads = {
             [3] = {["CLSID"] = "{AGM-154A}",},
         }
     },
-    ["AGM-154A x 2"] = {
+    ["AGM-154A x 2.regression"] = {
         ["pylons"] = {
             [3] = { ["CLSID"] = "{BRU55_2*AGM-154A}",},
         }
@@ -14,7 +14,7 @@ payloads = {
             [3] = {["CLSID"] = "{9BCC2A2B-5708-4860-B1F1-053A18442067}",},
         }
     },
-    ["AGM-154C x 2"] = {
+    ["AGM-154C x 2.regression"] = {
         ["pylons"] = {
             [3] = {["CLSID"] = "{BRU55_2*AGM-154C}",},
         }

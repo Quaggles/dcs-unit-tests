0def2ebe7819fd285b3554068d10f0b25b76e1ad
Quaggles <RyanQuagliata@gmail.com>
2023-08-09 07:26:48 +1000

    Added Mirage F1 EE Rocket tests from ShadowFrost

---
+++ ThirdParty/Mirage F1/EE/Rockets/HE/payloads.lua
@@ -0,0 +1,65 @@
+payloads = {
+    ["MF1-251"] = {
+        ["pylons"] = {
+            [5] = {["CLSID"] = "{MATRA_F1_SNEBT251}",},
+			[3] = {["CLSID"] = "{MATRA_F1_SNEBT251}",},
+        }
+    },
+   ["MF1-253"] = {
+        ["pylons"] = {
+            [5] = {["CLSID"] = "{MATRA_F1_SNEBT253}",},
+			[3] = {["CLSID"] = "{MATRA_F1_SNEBT253}",},
+        }
+    },
+		["MF1-256"] = {
+        ["pylons"] = {
+            [5] = {["CLSID"] = "{MATRA_F1_SNEBT256}",},
+			[3] = {["CLSID"] = "{MATRA_F1_SNEBT256}",},
+        }
+    },
+	["MF1-259E"] = {
+        ["pylons"] = {
+            [5] = {["CLSID"] = "{MATRA_F1_SNEBT259E}",},
+			[3] = {["CLSID"] = "{MATRA_F1_SNEBT259E}",},
+        }
+    },
+	["MF4-251"] = {
+        ["pylons"] = {
+            [5] = {["CLSID"] = "{MATRA_F4_SNEBT251}",},
+			[3] = {["CLSID"] = "{MATRA_F4_SNEBT251}",},
+        }
+    },
+	["MF4-253"] = {
+        ["pylons"] = {
+            [5] = {["CLSID"] = "{MATRA_F4_SNEBT253}",},
+			[3] = {["CLSID"] = "{MATRA_F4_SNEBT253}",},
+        }
+    },
+	["MF4-256"] = {
+        ["pylons"] = {
+            [5] = {["CLSID"] = "{MATRA_F4_SNEBT256}",},
+			[3] = {["CLSID"] = "{MATRA_F4_SNEBT256}",},
+        }
+    },
+	["MF4-257"] = {
+        ["pylons"] = {
+            [5] = {["CLSID"] = "{MATRA_F4_SNEBT257}",},
+			[3] = {["CLSID"] = "{MATRA_F4_SNEBT257}",},
+        }
+    },
+	["MF4-252"] = {
+        ["pylons"] = {
+            [5] = {["CLSID"] = "{MATRA_F4_SNEBT252}",},
+			[3] = {["CLSID"] = "{MATRA_F4_SNEBT252}",},
+        }
+    },
+	["MF1-257"] = {
+        ["pylons"] = {
+            [5] = {["CLSID"] = "{MATRA_F1_SNEBT257}",},
+		    [3] = {["CLSID"] = "{MATRA_F1_SNEBT257}",},
+        }
+    }, 
+
+	
+}
+

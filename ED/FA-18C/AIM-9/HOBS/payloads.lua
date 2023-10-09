076ed6f0e428f3dfee4f03bd4e48a7142b6e25b2
Quaggles <RyanQuagliata@gmail.com>
2023-10-10 00:57:38 +1000

    Updated F/A-18C AIM-9 Bore-STT test

--- ED/FA-18C/AIM-9/HOBS/payloads.lua
+++ ED/FA-18C/AIM-9/HOBS/payloads.lua
@@ -5,13 +5,13 @@ payloads = {
             [9] = {["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",},
         }
     },
-    ["AIM-9X Wing Single"] = {
+    ["AIM-9X Wing Single.regression"] = {
         ["pylons"] = {
             [2] = {["CLSID"] = "LAU-115_LAU-127_AIM-9X",},
             [8] = {["CLSID"] = "LAU-115_LAU-127_AIM-9X",},
         }
     },
-    ["AIM-9X Wing Double"] = {
+    ["AIM-9X Wing Double.regression"] = {
         ["pylons"] = {
             [2] = {["CLSID"] = "LAU-115_2*LAU-127_AIM-9X",},
             [8] = {["CLSID"] = "LAU-115_2*LAU-127_AIM-9X",},

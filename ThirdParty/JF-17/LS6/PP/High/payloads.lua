b657b2be8db298e18f30b3621af3fe75443532d0
Quaggles <8382945+Quaggles@users.noreply.github.com>
2024-01-27 17:55:48 +1000

    Added new JF-17 LS6 Tests

---
+++ ThirdParty/JF-17/LS6/PP/High/payloads.lua
@@ -0,0 +1,28 @@
+payloads = {
+	["LS6-500"] = {
+        ["pylons"] = {
+            [2] = {["CLSID"] = "DIS_LS_6_500",},
+			[6] = {["CLSID"] = "DIS_LS_6_500",},
+        }
+    },
+	["LS6-250"] = {
+        ["pylons"] = {
+            [2] = {["CLSID"] = "DIS_LS_6_250",},
+			[6] = {["CLSID"] = "DIS_LS_6_250",},
+        }
+    },
+	["Dual LS6-250"] = {
+        ["pylons"] = {
+            [2] = {["CLSID"] = "DIS_LS_6_250_DUAL_L",},
+			[6] = {["CLSID"] = "DIS_LS_6_250_DUAL_L",},
+        }
+    },
+	["Dual LS6-100"] = {
+        ["pylons"] = {
+            [2] = {["CLSID"] = "DIS_LS_6_100_DUAL_L",},
+			[6] = {["CLSID"] = "DIS_LS_6_100_DUAL_L",},
+        }
+    },
+}
+
+

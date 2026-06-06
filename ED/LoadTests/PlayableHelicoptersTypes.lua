2d50dd97f3e30bb366bc6040a2215933471ad9f2
Quaggles <8382945+Quaggles@users.noreply.github.com>
2026-06-06 18:07:40 +1000

    Add liveries to load test files (Fixes F-14 crash)

--- ED/LoadTests/PlayableHelicoptersTypes.lua
+++ ED/LoadTests/PlayableHelicoptersTypes.lua
@@ -1,9 +1,9 @@
 types = {
-    "AH-64D_BLK_II",
-    "CH-47Fbl1",
-    "Ka-50",
-    "Ka-50_3",
-    "Mi-24P",
-    "Mi-8MT",
-    "UH-1H",
+    ["AH-64D_BLK_II"] = {livery = "jgsdf——1st_combat_helicopter_unit"},
+    ["CH-47Fbl1"] = {livery = "royal canadian air force"},
+    ["Ka-50"] = {livery = "turkey fictional light gray"},
+    ["Ka-50_3"] = {livery = "ka-50_black_neutral"},
+    ["Mi-24P"] = {livery = "af 440 ovp"},
+    ["Mi-8MT"] = {livery = "Russia_Naryan-Mar"},
+    ["UH-1H"] = {livery = "Australia RAAF 1968"},
 }
--- ThirdParty/LoadTests/PlayableHelicoptersTypes.lua
+++ ThirdParty/LoadTests/PlayableHelicoptersTypes.lua
@@ -1,6 +1,6 @@
 types = {
-    "SA342L",
-    "SA342M",
-    "SA342Minigun",
-    "OH58D",
+    ["OH58D"] = {livery = "us 1-17 b 381"},
+    ["SA342L"] = {livery = "uk army batus"},
+    ["SA342M"] = {livery = "uk army batus"},
+    ["SA342Minigun"] = {livery = "uk army batus"},
 }

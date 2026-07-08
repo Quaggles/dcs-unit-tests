07c0a40a2025815f234a29b867db46b6a4ee7b7a
Quaggles <8382945+Quaggles@users.noreply.github.com>
2026-07-08 18:16:13 +1000

    Rerecord A-10C CBU87/97 tests with correct fuze settings in ME

--- ED/A-10C_2/CBU/CCRP/payloads.lua
+++ ED/A-10C_2/CBU/CCRP/payloads.lua
@@ -1,12 +1,33 @@
 payloads = {
     ["CBU-87"] = {
         ["pylons"] = {
-            [6] = {["CLSID"] = "{CBU-87}",},
+            [6] = {
+                ["CLSID"] = "{CBU-87}",
+                ["settings"] = 
+                {
+                    ["function_delay_ctrl_FZU39_SUU65"] = 2.23,
+                    ["ang_vel_x"] = 104.7,
+                    ["function_altitude_ctrl_FZU39_SUU65"] = 274.32,
+                    ["NFP_PRESID"] = "CBU_SUU65Plus",
+                    ["NFP_fuze_type_nose"] = "FZU39",
+                    ["NFP_PRESVER"] = 1,
+                },
+            },
         }
     },
 	["CBU-97"] = {
         ["pylons"] = {
-            [6] = {["CLSID"] = "{5335D97A-35A5-4643-9D9B-026C75961E52}",},
+            [6] = {
+                ["CLSID"] = "{5335D97A-35A5-4643-9D9B-026C75961E52}",
+                ["settings"] = 
+                {
+                    ["NFP_PRESID"] = "MDRN_CC_A_SUU65Plus_SFW",
+                    ["function_altitude_ctrl_FZU39_SUU65_SFW"] = 274.32,
+                    ["function_delay_ctrl_FZU39_SUU65_SFW"] = 2.23,
+                    ["NFP_fuze_type_nose"] = "FZU39",
+                    ["NFP_PRESVER"] = 1,
+                },
+            },
         }
     },
 }

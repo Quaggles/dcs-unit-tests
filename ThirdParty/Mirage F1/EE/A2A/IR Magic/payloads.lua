b8ceb04ea9fa0fe2da8598801af56884efe69fc9
Quaggles <8382945+Quaggles@users.noreply.github.com>
2026-07-08 18:46:26 +1000

    Mirage F1 rerecord and reorganise IR tests

---
+++ ThirdParty/Mirage F1/EE/A2A/IR Magic/payloads.lua
@@ -0,0 +1,13 @@
+payloads = {
+	["R550 Magic 1"] = {
+        ["pylons"] = {
+            [1] = {["CLSID"] = "{R550_Magic_1}",},
+        }
+    },
+	["R550 Magic 2"] = {
+        ["pylons"] = {
+            [1] = {["CLSID"] = "{FC23864E-3B80-48E3-9C03-4DA8B1D7497B}",},
+        }
+    },
+}
+
--- ThirdParty/Mirage F1/EE/A2A/IR Sidewinder/payloads.lua
+++ ThirdParty/Mirage F1/EE/A2A/IR Sidewinder/payloads.lua
@@ -19,16 +19,5 @@ payloads = {
             [1] = {["CLSID"] = "{AIM-9JULI}",},
         }
     },
-	["R550 Magic 1"] = {
-        ["pylons"] = {
-            [1] = {["CLSID"] = "{R550_Magic_1}",},
-        }
-    },
-	["R550 Magic 2"] = {
-        ["pylons"] = {
-            [1] = {["CLSID"] = "{FC23864E-3B80-48E3-9C03-4DA8B1D7497B}",},
-        }
-    },
-
 }
 
